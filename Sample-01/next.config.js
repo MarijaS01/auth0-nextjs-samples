a0d69c7fec193804bc67376c2a3b528f019dd506
MarijaS01 <marija.shanina@gmail.com>
2026-02-07 18:15:00 +0200

    Update next.config.js
    changes for auth

--- Sample-01/next.config.js
+++ Sample-01/next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  poweredByHeader: false
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || process.env.APP_BASE_URL,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  },  
+}
+module.exports = nextConfig
